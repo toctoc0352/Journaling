cabe5d26be661e7e08dec28a4e8fc8828f5e1fe9
江上剛志 <egm352@gmail.com>
2019-05-25 01:09:27 +0900

    HelloWorldインテントを元に戻す

--- lambda/custom/journaling.py
+++ lambda/custom/journaling.py
@@ -29,6 +29,20 @@ class LaunchRequestHandler(AbstractRequestHandler):
         handler_input.response_builder.speak(speech_text).set_should_end_session(True)
         return handler_input.response_builder.response
 
+
+class HelloWorldIntentHandler(AbstractRequestHandler):
+     """Handler for Hello World Intent."""
+     def can_handle(self, handler_input):
+         # type: (HandlerInput) -> bool
+         return is_intent_name("HelloWorldIntent")(handler_input)
+
+     def handle(self, handler_input):
+         # type: (HandlerInput) -> Response
+         speech_text = "Hello World!"
+         handler_input.response_builder.speak(speech_text).set_should_end_session(True)
+         return handler_input.response_builder.response
+
+
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
     def can_handle(self, handler_input):
@@ -109,6 +123,7 @@ class ErrorHandler(AbstractExceptionHandler):
 # defined are included below. The order matters - they're processed top to bottom.
 sb = SkillBuilder()
 sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(HelloWorldIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
