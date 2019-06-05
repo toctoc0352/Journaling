110e7fc7038e242028720e94439ec4b674f800f0
江上剛志 <egm352@gmail.com>
2019-06-05 23:13:28 +0900

    起動時とTriggerIntentでセッションを継続するよう修正

--- lambda/custom/journaling.py
+++ lambda/custom/journaling.py
@@ -28,7 +28,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
         service = JournalingService()
         speech_text = service.greetingPhrase()
         handler_input.response_builder.speak(
-            speech_text)
+            speech_text).set_should_end_session(False)
         return handler_input.response_builder.response
 
 
@@ -44,7 +44,7 @@ class TriggerIntentIntentHandler(AbstractRequestHandler):
         service = JournalingService()
         speech_text = service.trigerPhrase()
         handler_input.response_builder.speak(
-            speech_text).set_should_end_session(True)
+            speech_text).set_should_end_session(False)
         return handler_input.response_builder.response
 
 
